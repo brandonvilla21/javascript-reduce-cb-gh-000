ec4e468092e948510d21d75c593d7b072abb1fe7
Brandon Villa <bornofos@gmail.com>
2018-01-15 20:15:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const app = "I don't do much."
+const couponLocations = [
+  { room: 'Living room', amount: 5 },
+  { room: 'Kitchen', amount: 2 },
+  { room: 'Bathroom', amount: 1 },
+  { room: 'Master bedroom', amount: 7 }
+];
+
+const res = couponLocations.reduce((totalAmount, location) => {
+  return totalAmount + location.amount;
+}, 0); // also prints 15!
