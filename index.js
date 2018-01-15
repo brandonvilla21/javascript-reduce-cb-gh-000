79fc5568ef53075563ed74f87a30397ca955dde2
Brandon Villa <bornofos@gmail.com>
2018-01-15 20:11:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const app = "I don't do much."
+const couponLocations = [
+  { room: 'Living room', amount: 5 },
+  { room: 'Kitchen', amount: 2 },
+  { room: 'Bathroom', amount: 1 },
+  { room: 'Master bedroom', amount: 7 }
+];
+
+function couponCounter(totalAmount, location) {
+  return totalAmount + location.amount;
+}
+
+console.log(couponLocations.reduce((totalAmount, location, 0) => {
+  return totalAmount + location.amount;
+})); // also prints 15!
